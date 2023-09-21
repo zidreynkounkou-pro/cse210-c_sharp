c37acc0ae2797ebf4b25060bcd4387200a556540
zidreynkounkou-pro <relgazidreynkounkou@mail.com>
2023-09-21 04:56:45 +0100

    Worked on conditions in C#.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string user = Console.ReadLine();
+        int intUser = int.Parse(user);
+
+        string letter;
+
+        if (intUser >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (intUser >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (intUser >= 70)
+        {
+            letter = "C";
+        }
+
+
+        else if (intUser >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (intUser >= 70)
+        {
+            Console.WriteLine("Congratulatons, you passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("We are sorry that you failed, keep up the good work and come again!");
+        }
     }
 }
