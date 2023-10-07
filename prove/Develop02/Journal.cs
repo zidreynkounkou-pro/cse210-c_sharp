26f3ee069f15a7428ba3066234608777156b2b50
zidreynkounkou-pro <relgazidreynkounkou@mail.com>
2023-10-07 21:09:33 +0100

    W04-Prove-Journal.

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -6,6 +6,9 @@ public class Journal {
 
   public string _save;
   public string _load;
+  public string _format;
+  public string _loadFormat;
+  public string _ex;
 
   public List<Entry> entries = new List<Entry>();
   public void AddEntry(Entry entry)
@@ -23,32 +26,62 @@ public class Journal {
 
   public void WritingFile()
   {
-   
-      using (StreamWriter _writer = new StreamWriter(_save, true))
+      // I decided to write the file with wether .csv or .text formats according to the user's need.
+      using (StreamWriter _writer = new StreamWriter(_save + "."+ _format, true))
     {
       foreach (Entry _file in entries)
       {
         _writer.WriteLine(_file.StoreEntries());
       }
     }
-
   }
 
 
   public void LoadingFile()
   {
-    
-    string[] _lines = System.IO.File.ReadAllLines(_load);
-    foreach (string _line in _lines)
+    try // Handling Exceptions
     {
-      string[] _parts = _line.Split(" | ");
-      string _date = _parts[0];
-      string _prompt = _parts[1];
-      string _response = _parts[2];
+      string[] _lines = System.IO.File.ReadAllLines(_load + "." + _loadFormat);
+    
+    
+      Console.WriteLine("Your entries are:\n");
 
-      Console.WriteLine($"{_date}\n{_prompt}\n{_response}");
+      foreach (string _line in _lines)
+      {
+        string[] _parts = _line.Split(" | ");
+        string _date = _parts[0];
+        string _prompt = _parts[1];
+        string _response = _parts[2];
+
+        Console.WriteLine($"{_date}\n{_prompt}\n{_response}");
+        Console.WriteLine();
+      }
+    }
+
+    catch (NotSupportedException)
+    {
+      Console.WriteLine($"The format '.{_loadFormat}'not supported.");
+      Console.WriteLine();
+    }
+    catch (FileNotFoundException)
+    {
+      Console.WriteLine($"The file '{_load + '.' + _loadFormat}' not found. Check your file name and format, then try again.");
+      Console.WriteLine();
+    }
+    catch (DirectoryNotFoundException)
+    {
+      Console.WriteLine($"The file '{_load + '.' + _loadFormat}' directory not found.");
+      Console.WriteLine();
+    }
+    catch(IOException e)
+    {
+      Console.WriteLine(e);
       Console.WriteLine();
     }
   }
 
+  public string HandlingFormatException()
+  {
+    return _ex;
+  }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,6 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+
+
+/* 
+To exceed requirements for this projet, I have covered the following:
+- I have allowed the user to give only the name of the file first, then being prompted to give the format that they desir to save their file with. csv and text formats being recommanded.
+- I have handeled exceptions that may occur when interacting with the user. Such as FormatException for manipulating the menu
+(Here I set an attribute and created a method that allow me to pick up the string, then print it on the screen for the user view.), FileNotFoundException, DirectoryNotFoundException, NotSupportedException ....
+
+All these adds have led me to create other attributes and methods that you can find below and in the Journal class.
+
+I have done this project, so it is more dynimic.
+*/
 
 class Program
 {
@@ -16,9 +26,10 @@ class Program
         // Setting the Journal class instance.
         Journal journal = new Journal();
         
-        int toDo = -1;
-        while (toDo != 5)
-       {
+        
+        int _toDo = -1;
+        while (_toDo != 5)
+        {
             Console.WriteLine("Please select one of the following: ");
             Console.WriteLine("");
         
@@ -30,52 +41,72 @@ class Program
 
             Console.WriteLine();
             Console.Write("What would you like to do? ");
+
             // Getting the user's input.
-            toDo = int.Parse(Console.ReadLine());
-            Console.WriteLine();
+            try // Handling format exception.
+            {     string _exception = Console.ReadLine();
+            journal._ex = _exception;
+                _toDo = int.Parse(_exception);
 
-            // Let's initialize the time, then get the current date.
-            DateTime dateTime = DateTime.Now;
-            string currentDate = dateTime.ToShortDateString();
-
-            // Checking if the user enters 1. (write)
-            if (toDo == 1)
-            {  
-                string prompt = PromptGenerator.RandomPrompt();
-                Console.WriteLine(prompt);
-                string response = Console.ReadLine();
                 Console.WriteLine();
+            
+            
+                // Let's initialize the time, then get the current date.
+                DateTime dateTime = DateTime.Now;
+                string currentDate = dateTime.ToShortDateString();
 
-                // The instance of the Entry class.
-                Entry entry = new Entry(currentDate, prompt, response);
+                // Checking if the user enters 1. (write)
+                if (_toDo == 1)
+                {  
+                    string prompt = PromptGenerator.RandomPrompt();
+                    Console.WriteLine(prompt);
+                    string response = Console.ReadLine();
+                    Console.WriteLine();
 
-                // Let's call the AddEntry method and pass it its parameters.
-                journal.AddEntry(entry); 
+                    // The instance of the Entry class.
+                    Entry entry = new Entry(currentDate, prompt, response);
+
+                    // Let's call the AddEntry method and pass it its parameters.
+                    journal.AddEntry(entry); 
+                }
+
+                else if (_toDo == 2)
+                {   // Calling the display method from the Journal class.
+                    journal.DisplayAllEntries();
+                }
+                else if (_toDo == 3)
+                {
+                    Console.WriteLine("What is the name of the file you would like to load? e.g: filename, myFile....");
+                    string _fileToLoad = Console.ReadLine();
+                    journal._load = _fileToLoad;
+                    Console.WriteLine("What is the format of your file? e.g: csv, text....");
+                    string _loadFormat = Console.ReadLine();
+                    journal._loadFormat = _loadFormat;
+                    Console.WriteLine();
+                    journal.LoadingFile();
+                }
+
+                else if (_toDo == 4)
+                {   Console.WriteLine("What is the name of the file you would like to save? e.g: myFile, filename....");
+                    string _fileName = Console.ReadLine();
+                    journal._save = _fileName;
+                    Console.WriteLine("What is the format of the file, please? e.g: csv, text.... (csv and text formats are recommanded.)");
+                    string _format = Console.ReadLine();
+                    journal._format = _format;
+                    Console.WriteLine();
+                    
+                    journal.WritingFile();
+                    Console.WriteLine($"Your file '{_fileName + "." + _format}' has been saved successfully.");
+                    Console.WriteLine();
+                }
             }
-
-            else if (toDo == 2)
-            {   // Calling the display method from the Journal class.
-                journal.DisplayAllEntries();
-            }
-            else if (toDo == 3)
+            catch (FormatException)
             {
-                Console.WriteLine("What is the name of the file you would like to load? ");
-                string _fileToLoad = Console.ReadLine();
-                journal._load = _fileToLoad;
+                Console.WriteLine($"Attempted to enter a wrong format '{journal.HandlingFormatException()}'. Please, enter an integer and try again!");
                 Console.WriteLine();
-
-                journal.LoadingFile();
-            }
-
-            else if (toDo == 4)
-            {   Console.WriteLine("What is the name of the file you would like to save? ");
-                String _fileName = Console.ReadLine();
-                journal._save = _fileName;
-                Console.WriteLine();
-
-                journal.WritingFile();
             }
        }  
+       Console.WriteLine("Thank you, bye-bye!");
        
     }
 }
