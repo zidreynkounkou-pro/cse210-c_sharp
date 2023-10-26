6bb95aa8de7f231e929e586972af63a187d419ab
zidreynkounkou-pro <relgazidreynkounkou@mail.com>
2023-10-26 11:55:12 +0100

    W07 Learning activity

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,19 @@
+public class MathAssignment: Assignment
+{
+  private string _textBookSection;
+  private string _problems;
+
+  public MathAssignment(string studentName, string topic, string textBookSection, string problems) : base(studentName, topic)
+
+  {
+    _textBookSection = textBookSection;
+    _problems = problems;
+  }
+
+  public string GetHomeworkList()
+  {
+    return $"Section {_textBookSection} Problems {_problems}";
+  }
+
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Console.WriteLine();
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine($"{math.GetSummary()}\n{math.GetHomeworkList()}");
+        Console.WriteLine();
+        
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History","The Causes of World War II");
+        Console.WriteLine($"{writing.GetSummary()}\n{writing.GetWritingInformation()}");
+
     }
 }
